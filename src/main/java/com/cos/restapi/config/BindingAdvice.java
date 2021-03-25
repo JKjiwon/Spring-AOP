a9dbdf4d06e494ac3d7ac9eef58cae243f65ea01
kimjiwon <jwkim9205@gmail.com>
2021-03-25 23:11:27 +0900

    프로젝트 내부 파일 로깅 & Sentry.io 로깅

--- src/main/java/com/cos/restapi/config/BindingAdvice.java
+++ src/main/java/com/cos/restapi/config/BindingAdvice.java
@@ -1,19 +1,25 @@
 package com.cos.restapi.config;
 
 import com.cos.restapi.domain.ResponseDto;
-import lombok.Builder;
+import io.sentry.Sentry;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+
 
 // @Component -> Controller 가 뜨고 나서
 // @Configuration -> Controller 가 뜨기
@@ -21,10 +27,15 @@ import java.util.Map;
 @Aspect
 public class BindingAdvice {
 
+    public static final Logger log = LoggerFactory.getLogger(BindingAdvice.class);
+
     // Around의 proceedingJoinPoint.proceed(); 이 실행 되어야 @Before이 실행된다.
     // 즉, Around 코드 중 에러가 발생하면 @Before이 발생하지 않는다
     @Before("execution(* com.cos.restapi.web..*Controller.*(..))")
     public void testCheck() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        System.out.println("주소: " + request.getRequestURL());
+
         // request 값을 처리 못하나요?
         // log 처리는? 파일로 남겨야 하는데....
         System.out.println("로그를 남겼습니다.");
@@ -52,10 +63,19 @@ public class BindingAdvice {
             if (arg instanceof BindingResult) {
                 BindingResult bindingResult = (BindingResult) arg;
 
+                // 서비스 : 정상적인 화면 -> 사용자 요청
                 if (bindingResult.hasErrors()) {
                     Map<String, String> errMap = new HashMap<>();
                     for (FieldError error : bindingResult.getFieldErrors()) {
                         errMap.put(error.getField(), error.getDefaultMessage());
+
+                        // 로그 레벨 error, warn, info, debug
+                        log.warn(type + "." + method + "() => 필드 : " + error.getField() + ", 메시지 : " + error.getDefaultMessage());
+                        log.debug(type + "." + method + "() => 필드 : " + error.getField() + ", 메시지 : " + error.getDefaultMessage());
+                        Sentry.captureMessage(type + "." + method + "() => 필드 : " + error.getField() + ", 메시지 : " + error.getDefaultMessage());
+
+                        // DB연결 -> DB남기기
+                        // File file = new File();
                     }
                     return new ResponseDto<>(HttpStatus.BAD_REQUEST.value(), errMap);
                 }
--- src/main/java/com/cos/restapi/config/GlobalExceptionHandler.java
+++ src/main/java/com/cos/restapi/config/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.cos.restapi.config;
 
 import com.cos.restapi.domain.ResponseDto;
+import io.sentry.Sentry;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = UserNotFoundException.class)
     public ResponseDto<String> handleDatabaseNotFoundException(UserNotFoundException e) {
+        Sentry.captureException(e);
         return new ResponseDto<>(HttpStatus.BAD_REQUEST.value(), e.getMessage());
     }
 }
